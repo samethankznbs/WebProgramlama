e91b1d276e863ce9dcb846458cde248f5ee68d3a
tugbaceviker <144686539+tugbaceviker@users.noreply.github.com>
2024-06-01 15:25:35 +0300

    Add Migrations

---
+++ WebProgramlama/DataAccessLayer/Migrations/20231130171756_antrenordanisaneslestirme.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class antrenordanisaneslestirme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppAntrenorId",
+                table: "Danisans",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Danisans_AppAntrenorId",
+                table: "Danisans",
+                column: "AppAntrenorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Danisans_Antrenors_AppAntrenorId",
+                table: "Danisans",
+                column: "AppAntrenorId",
+                principalTable: "Antrenors",
+                principalColumn: "AntrenorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Danisans_Antrenors_AppAntrenorId",
+                table: "Danisans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Danisans_AppAntrenorId",
+                table: "Danisans");
+
+            migrationBuilder.DropColumn(
+                name: "AppAntrenorId",
+                table: "Danisans");
+        }
+    }
+}
---
+++ WebProgramlama/DataAccessLayer/Migrations/20231202202721_guncelle.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class guncelle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "programSuresi",
+                table: "EgzersizProgramis",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "planSuresi",
+                table: "BeslenmePlanis",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "programSuresi",
+                table: "EgzersizProgramis",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "planSuresi",
+                table: "BeslenmePlanis",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
