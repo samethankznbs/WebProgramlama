6757ba004e6bb9e87eaa9445f92ebe8b835cba02
samethankznbs <100464602+samethankznbs@users.noreply.github.com>
2024-06-01 15:24:11 +0300

    Add Data Access Layer

---
+++ WebProgramlama/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,50 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=tcp:projeaylin.database.windows.net,1433;Initial Catalog=y;Integrated Security=False;User Id=sqladmin@projeaylin;Password=1234Aylin...;Encrypt=True;TrustServerCertificate=False;MultipleActiveResultSets=True");
+        }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public Context()
+        {
+        }
+
+        public DbSet<AppUser> appusers { get; set; }
+        public DbSet<mesaj> mesajlar { get; set; }
+       
+        public DbSet<AppAntrenor> Antrenors { get; set; }
+
+        public DbSet<AppDanisan> Danisans { get; set; }
+
+        public DbSet<EgzersizProgrami> EgzersizProgramis { get; set; }
+
+        public DbSet<BeslenmePlani> BeslenmePlanis { get; set; }
+
+     
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+          
+
+
+        }
+
+
+
+
+    }
+}
---
+++ WebProgramlama/DataAccessLayer/EntityFramework/EFDanisanRepository.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EFDanisanRepository : GenericRepository<AppDanisan>, IDanisanDal
+    {
+        public List<AppDanisan> GetListDanisanWithByAntrenor(int id)
+        {
+            using (var c = new Context())
+            {
+                return c.Danisans.Where(x => x.AppAntrenorId == id).ToList();
+            }
+        }
+    }
+}
